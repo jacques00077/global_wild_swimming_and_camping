4f4424c912329f7f47111610b2ece83ffb97fa64
jacques00077 <134284505+jacques00077@users.noreply.github.com>
2024-07-10 00:19:40 +0000

    Delete connection.php

--- connection.php
+++
@@ -1,8 +0,0 @@
-<?PHP
-$user_name = "root";
-$password = "";
-$database = "addressbook";
-$server = "127.0.0.1";
-mysqli_connect($server, $user_name, $password);
-print "Connection to the Server opened";
-?>
